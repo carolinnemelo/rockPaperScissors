026bd17e5d30ac677f017b7a61dffa73d0d97c80
Hans-Olov <hobornemann@gmail.com>
2023-11-23 23:47:21 +0100

    counting hands animation working

--- scripts/animation-test.js
+++ scripts/animation-test.js
@@ -3,36 +3,211 @@
 
 const triggerAnimationBtn = document.querySelector("#trigger-animation-button");
 
-
-triggerAnimationBtn.addEventListener("click", () =>{
-    countingHands();
+triggerAnimationBtn.addEventListener("click", () =>{   
+    playAPoint();
 });
+    
 
-function countingHands (){
-    const battlingHands = document.querySelectorAll(".battling-hand");  
-    /* If any element is member of the "counting-hand" class, remove the class?  */
-    if(!(battlingHands === 0)){
-        for(let i = 0; i < battlingHands.length; i++){
-            battlingHands[i].classList.remove("counting-hand");
-        }    
-    }
-    /* Add the "counting-hand" class (with the counting-animation) to all elements who are members of the "battling-hand" class. This will trigger the animation*/
-    for(let i = 0; i < battlingHands.length; i++){
-        battlingHands[i].classList.add("counting-hand");
-    }
-    /* use random function to decide the chosen hand by the computer */
 
-    /* determine who won the point */
+async function playAPoint(){
+    console.log("started to play a point");
+    const result1 = await removeCountingHandClass();
+    console.log(result1);
+    const result2 = await addCountingHandClass();
+    console.log(result2);
 
-    /* update the score */
-
-    /* check if someone has won the set and/or the match */
-
-    /* communicate if som */
 
 }
 
 
 
-/* battlingHands or another class name that has been put on the two battling hands - jpg.files   */
 
+
+  /* ----------------------------------------------------------------------- */ 
+  /* async function asyncCall() {
+    console.log('calling');
+    const result2 = await resolveAfter4Seconds();
+    console.log(result2);
+    const result1 = await resolveAfter2Seconds();
+    console.log(result1);
+  }
+  
+  
+  function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('2 seconds');
+      }, 2000);
+    });
+  }
+  function resolveAfter4Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('4 seconds');
+      }, 4000);
+    });
+  }
+  
+ 
+  asyncCall();
+  console.log('test'); */
+  /* ----------------------------------------------------------------------- */
+
+
+
+
+function addCountingHandClass(){
+    
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let battlingHandLeft = document.querySelector("#battling-hand-left");
+            let battlingHandRight = document.querySelector("#battling-hand-right");
+            battlingHandLeft.classList.add("counting-hand");
+            battlingHandRight.classList.add("counting-hand");  
+            resolve('addition of counting-hand class has been initiated');
+        }, 0);
+    });  
+};
+
+
+function addCountingHandClass2(){
+    
+    return new Promise(resolve => {
+            let battlingHandLeft = document.querySelector("#battling-hand-left");
+            let battlingHandRight = document.querySelector("#battling-hand-right");
+            battlingHandLeft.classList.add("counting-hand");
+            battlingHandRight.classList.add("counting-hand");  
+            resolve('addition of counting-hand class has been initiated');
+    });  
+};
+
+
+function removeCountingHandClass2(){
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let battlingHandLeft = document.querySelector("#battling-hand-left");
+            battlingHandLeft.classList.remove("counting-hand");
+            let battlingHandRight = document.querySelector("#battling-hand-right");
+            battlingHandRight.classList.remove("counting-hand");
+          resolve("removal of counting-hand class has been initiated");
+        }, 2000);
+      });
+    ;
+};
+    
+
+function removeCountingHandClass(){
+
+    return new Promise(resolve => {
+        let countingHandClassHasBeenRemoved = false;
+        while(countingHandClassHasBeenRemoved === false){
+            countingHandClassHasBeenRemoved = setTimeout(hasCountingHandClassBeenRemoved(),500);
+            let battlingHandLeft = document.querySelector("#battling-hand-left");
+            battlingHandLeft.classList.remove("counting-hand");
+            let battlingHandRight = document.querySelector("#battling-hand-right");
+            battlingHandRight.classList.remove("counting-hand");
+        }
+        resolve("removal of counting-hand class has been completed");
+      });
+};
+    
+
+
+function hasCountingHandClassBeenRemoved (){
+    let countingHands = document.querySelectorAll(".counting-hand");
+    console.log(countingHands);
+    if(countingHands === null){
+        return true;
+    } else {
+        return false;
+    }
+};
+
+    
+        
+
+
+function computerChoosesHand(response){
+
+     /* use random function to decide the chosen hand by the computer */
+
+    return response;      
+};
+
+
+function decideWinnerAndUpdateScore(response){
+
+        /* determine who won the point */
+        /* update the score */
+
+    /* check if someone has won the set and/or the match */
+
+    /* communicate if som */
+
+    return response;
+};
+
+
+
+/* Example code */
+/* async function calculateResults(){
+    try {
+        const response = await result(80)
+        console.log("Results received")
+        const finalgrade = await grade(response)
+         console.log(finalgrade)
+        }
+
+    catch(err){
+            console.log(err)
+        }
+}
+
+calculateResults() */
+
+
+
+ 
+
+/* async function playAPoint(){
+
+    try {
+        const countingHandClassRemoved = await removeCountingHandClass(countingHandClassAdded);
+        alert("Has removeCountingHandClass been executed? "+ countingHandClassRemoved);
+        const countingHandClassAdded = await addCountingHandClass();
+        const computerHasChosenHand = await computerChoosesHand(countingHandClassRemoved);
+        const winnerDecidedAndScoreUpdated = await decideWinnerAndUpdateScore(computerHasChosenHand);
+        }
+
+    catch(err){
+            alert(err)
+        }
+}; */
+
+
+
+
+/* alert("Counting-hand class has been added: "+response); */
+    /* console.log("Counting-hand class has been added: "+response); */
+    /* return response; */
+
+
+    /* let battlingHands = document.querySelectorAll(".battling-hand"); 
+    battlingHands.forEach(element =>{
+        element.classList.add("counting-hand"); 
+    }); */
+
+
+    
+   
+
+    /* alert("Counting-hand class has been removed: "+response); */
+    /*  console.log("Counting-hand class has been removed: "+response); */
+    /* return response; */
+
+
+  /* let countingHands = document.querySelectorAll(".counting-hand"); 
+        countingHands.forEach(element =>{
+            element.classList.remove("counting-hand"); 
+        }); */
