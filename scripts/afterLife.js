1be706430bb4e34ba7c0834f39fad9a9c2b6d534
Carolinne Melo <carolinnepmelo@gmail.com>
2023-11-24 13:59:26 +0100

    decide who won the round

--- scripts/afterLife.js
+++ scripts/afterLife.js
@@ -1,81 +1,81 @@
-//let gamerCharacter = sessionStorage.getItem("characterChoice");
-let gamerCharacter = "mysteryHuman";
-let computerCharacter = "";
-let computerWeapon = "";
 //let nickName = sessionStorage.getItem("nickName");
 let nickName = "Edda";
 
-const cardsData = {
-    smellyCat: {
-        name: "Smelly Cat",
-        powerDescription: "Scent Conjurer",
-        image: "/images/player-cat.png"
-    },
-    skellington: {
-        name: "Skellington",
-        powerDescription: "Bone Control",
-        image: "/images/player-skeleton.png"
-    },
-    mysteryHuman: {
-        name: "Mystery Human",
-        powerDescription: "Speedy Fingers",
-        image: "/images/player-mysteryman.png"
-    }
-};
 
-const characterData = {
-    smellyCat: [
-        "/images/rock-cat.png",
-        "/images/paper-cat.png",
-        "/images/scissors-cat.png"
-    ],
-    skellington: [
-        "/images/rock-skeleton.png",
-        "/images/paper-skeleton.png",
-        "/images/scissors-skeleton.png"
-    ],
-    mysteryHuman: [
-        "/images/rock-human.png",
-        "/images/paper-human.png",
-        "/images/scissors-human.png"
-    ]
-};
+async function loadData() {
+    const response = await fetch("/data/charactersData.json");
+    return await response.json();
+}
 
-function assignComputerCharacter () {
-    let randomNumber = Math.floor(Math.random()* 3);
-    if (randomNumber === 0) {
-        computerCharacter = "smellyCat"
-    } else if (randomNumber === 1) {
-        computerCharacter = "skellington"
-    } else {
-        computerCharacter = "mysteryHuman"
-    }
-};
-
-function iscomputerWeapon(computerCharacter) {
-    let randomWeapon = Math.floor(Math.random()* 3);
-    computerWeapon = characterData.computerCharacter[randomWeapon];
-
-};
-
-window.onload = function() {
+function assignComputerCharacter (characters) {
+    let computerCharacterIndex = Math.floor(Math.random() * characters.length);
+    return characters[computerCharacterIndex];
     
-    assignComputerCharacter();
-    iscomputerWeapon();
+};
 
+function getCharacterById(id, characters) {
+    for (let character of characters) {
+        if (character.id === id) {
+            return character;
+        }
+    }
+}
+
+function chooseComputerWeapon(weapons) {
+    let randomWeapon = Math.floor(Math.random()* weapons.length);
+    let computerWeaponIndex = randomWeapon;
+    return weapons[computerWeaponIndex];
+};
+
+function whoIsTheWinner (computerWeapon, gamerWeapon) {
+    if ((computerWeapon === 'paper' && gamerWeapon === 'rock') || 
+        (computerWeapon === 'scissors' && gamerWeapon === 'paper') ||
+        (computerWeapon === 'rock' && gamerWeapon === 'scissors')) {
+            return "computer"
+        }else if (computerWeapon === gamerWeapon) {
+            return "tie";
+        } else {
+            return "gamer"
+        }
+};
+
+
+window.onload = async function() {
+
+    const WEAPONS = ["rock", "paper", "scissors"];
+
+    let characters = await loadData();
+    let computerCharacter = assignComputerCharacter(characters);
+
+    //let gamerCharacter = sessionStorage.getItem("characterChoice");
+    let gamerCharacterId = "mysteryHuman";
+    //let gamerWeapon = sessionStorage.getItem("weapon"); 
+    let gamerWeapon = "paper"; 
+
+    let gamerCharacter = getCharacterById(gamerCharacterId, characters);
+    let computerWeapon = chooseComputerWeapon(WEAPONS);
+
+    let winner = whoIsTheWinner(computerWeapon, gamerWeapon);
+
+    console.log(winner);
+
+    
     let characterImagePlace = document.querySelector("#characterChoice");
     let characterNamePlace = document.querySelector("#characterNamePlace");
+
+    
     let nickNamePlace = document.querySelector("#nickNamePlace");
     let computerCharacterNamePlace = document.querySelector("#computerCharacterNamePlace");
     let computerCharacterImagePlace = document.querySelector("#computerChoice");
-
-
-
-    characterImagePlace.src = cardsData[gamerCharacter].image;
-    characterNamePlace.textContent = cardsData[gamerCharacter].name;
+    
+    
+    
+    characterImagePlace.src = gamerCharacter.image;
+    characterNamePlace.textContent = gamerCharacter.name;
     nickNamePlace.textContent = nickName;
-    computerCharacterNamePlace.textContent = cardsData[computerCharacter].name;
-    computerCharacterImagePlace.src = cardsData[computerCharacter].image;
+    computerCharacterNamePlace.textContent = computerCharacter.name;
+    computerCharacterImagePlace.src = computerCharacter.image;
 
+
+    
 };
-
